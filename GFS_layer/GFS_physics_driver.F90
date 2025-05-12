596a5c4cc4e2dcfa49c35008f2e39e5bdf3aba7a
Wolfgang Langhans <Wolfgang.Langhans@sofarocean.com>
2025-05-12 12:40:36 -0700

    Diagnose 10-m actual wind and neutral wind in sfc_diag for both diagnostic output and sfcprop; fix bug in computation of 10-m neutral wind components

--- GFS_layer/GFS_physics_driver.F90
+++ GFS_layer/GFS_physics_driver.F90
@@ -479,7 +479,7 @@ module module_physics_driver
            doms, psautco_l, prautco_l, ocalnirbm_cpl, ocalnirdf_cpl,    &
            ocalvisbm_cpl, ocalvisdf_cpl, dtzm, temrain1, t2mmp, q2mp,   &
            !--  For coupling with wave model                              ! Sofar added 10/20/23
-           fm10_neutral,                                                & ! Sofar added 10/20/23
+           fm10_neutral, fm_neutral,                                    & ! Sofar added 10/20/23
            !--- coupling inputs for physics
            dtsfc_cice, dqsfc_cice, dusfc_cice, dvsfc_cice, ulwsfc_cice, &
            tisfc_cice, tsea_cice, hice_cice, fice_cice,                 &
@@ -1217,10 +1217,8 @@ module module_physics_driver
                  Sfcprop%tsfc, Sfcprop%zorl, Sfcprop%ztrl, cd,      &
                  cdq, rb, Statein%prsl(1,1), work3, islmsk, stress, &
                  Sfcprop%ffmm,  Sfcprop%ffhh,                       &
-                 Sfcprop%charnock,                                  &  ! Sofar added Spring 2023
-                 Sfcprop%u10m, Sfcprop%v10m,                        &  ! Sofar added 11/17/23
-                 Sfcprop%u10n, Sfcprop%v10n,                        &  ! Sofar added 9/22/23
-                 fm10_neutral,                                      &  ! Sofar added 10/19/23
+                 Sfcprop%charnock,                                  &    
+                 fm10_neutral, fm_neutral,                          &  
                  Sfcprop%uustar,                                    &
                  wind,  Tbd%phy_f2d(1,Model%num_p2d), fm10, fh2,    &
                  sigmaf, vegtype, Sfcprop%shdmax, Model%ivegsrc,    &
@@ -1506,7 +1504,9 @@ module module_physics_driver
               Sfcprop%f10m, Diag%u10m, Diag%v10m,        &
               Sfcprop%t2m, Sfcprop%q2m, work3, evap,           &
               Sfcprop%ffmm, Sfcprop%ffhh, fm10, fh2, &
-              fm10_neutral, Diag%u10n, Diag%v10n, Sfcprop%rhoa)  ! Added by Sofar: 10/19/23
+              Sfcprop%u10m, Sfcprop%v10m,&
+              fm10_neutral, fm_neutral, Diag%u10n, Diag%v10n, &
+              Sfcprop%u10n, Sfcprop%v10n, Sfcprop%rhoa)  
       !endif
 
       Tbd%phy_f2d(:,Model%num_p2d) = 0.0
@@ -3865,7 +3865,9 @@ module module_physics_driver
                        Sfcprop%f10m, Diag%u10m, Diag%v10m, Sfcprop%t2m, &
                        Sfcprop%q2m, work3, evap, Sfcprop%ffmm,          &
                        Sfcprop%ffhh, fm10, fh2, &
-                       fm10_neutral, Diag%u10n, Diag%v10n, Sfcprop%rhoa)  ! Added by Sofar: 10/19/23
+                       Sfcprop%u10m, Sfcprop%v10m,&
+                       fm10_neutral, fm_neutral, Diag%u10n, Diag%v10n, &
+                       Sfcprop%u10n, Sfcprop%v10n, Sfcprop%rhoa)  
 
         if (Model%lssav) then
           Diag%tmpmax (:) = max(Diag%tmpmax (:),Sfcprop%t2m(:))
