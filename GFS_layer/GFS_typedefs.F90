f7694c9cb95a76a46feb13c205c77ba0cbb6a4d0
Wolfgang Langhans <Wolfgang.Langhans@sofarocean.com>
2025-05-13 17:00:23 -0700

    Added parameter tune_ocean_surface_layer to switch on/off the tuning of the SL layer over oceans

--- GFS_layer/GFS_typedefs.F90
+++ GFS_layer/GFS_typedefs.F90
@@ -679,6 +679,7 @@ module GFS_typedefs
     real(kind=kind_phys) :: wind_th_hwrf    !< wind speed threshold when z0 level off as in HWRF (kgao)
     real(kind=kind_phys) :: alpha_stable    !< description TBD
     real(kind=kind_phys) :: alpha_unstable  !< description TBD
+    logical              :: tune_ocean_surface_layer !< if true, use alphas above to modify the SL similarity profiles over oceans
     logical              :: hybedmf         !< flag for hybrid edmf pbl scheme
     logical              :: myj_pbl         !< flag for NAM MYJ tke scheme
     logical              :: ysupbl          !< flag for ysu pbl scheme (version in WRFV3.8)
@@ -2396,6 +2397,7 @@ end subroutine overrides_create
     real(kind=kind_phys) :: wind_th_hwrf   = 33.                      !< wind speed threshold when z0 level off as in HWRF
     real(kind=kind_phys) :: alpha_stable   = 5.                       !< TBD
     real(kind=kind_phys) :: alpha_unstable = 16.                      !< TBD
+    logical              :: tune_ocean_surface_layer = .false.        !< if true, use alphas above to modify the SL similarity profiles over oceans
     logical              :: hybedmf        = .false.                  !< flag for hybrid edmf pbl scheme
     logical              :: myj_pbl        = .false.                  !< flag for NAM MYJ tke-based scheme
     logical              :: ysupbl         = .false.                  !< flag for hybrid edmf pbl scheme
@@ -2626,8 +2628,9 @@ end subroutine overrides_create
                                ras, trans_trac, old_monin, cnvgwd, mstrat, moist_adj,       &
                                cscnv, cal_pre, do_aw, do_shoc, shocaftcnv, shoc_cld,        &
                                h2o_phys, pdfcld, shcnvcw, redrag, sfc_gfdl, z0s_max,        &
-                               do_z0_moon, do_z0_hwrf15, do_z0_hwrf17,                      &
-                               do_z0_hwrf17_hwonly, wind_th_hwrf,                           &
+                               sfc_coupled, do_z0_moon, do_z0_hwrf15, do_z0_hwrf17,         &
+                               do_z0_hwrf17_hwonly, wind_th_hwrf, alpha_stable,             &
+                               alpha_unstable, tune_ocean_surface_layer,                    &                                             &
                                hybedmf, dspheat, lheatstrg, hour_canopy, afac_canopy,       &
                                cnvcld, no_pbl, xkzm_lim, xkzm_fac, xkgdx,                   &
                                rlmn, rlmx, zolcru, cs0,                                     &
@@ -2874,6 +2877,7 @@ end subroutine overrides_create
     Model%wind_th_hwrf     = wind_th_hwrf
     Model%alpha_stable     = alpha_stable
     Model%alpha_unstable   = alpha_unstable
+    Model%tune_ocean_surface_layer = tune_ocean_surface_layer
     Model%hybedmf          = hybedmf
     Model%myj_pbl          = myj_pbl
     Model%ysupbl           = ysupbl
@@ -3581,6 +3585,7 @@ end subroutine overrides_create
       print *, ' wind_th_hwrf      : ', Model%wind_th_hwrf
       print *, ' alpha_stable      : ', Model%alpha_stable
       print *, ' alpha_unstable    : ', Model%alpha_unstable
+      print *, ' tune_ocean_surface_layer : ', Model%tune_ocean_surface_layer 
       print *, ' hybedmf           : ', Model%hybedmf
       print *, ' myj_pbl           : ', Model%myj_pbl
       print *, ' ysupbl            : ', Model%ysupbl
