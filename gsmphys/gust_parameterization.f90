3f20bffed1ccbd96da60ce3fd2e176a00538202c
Wolfgang Langhans <wolfgang.langhans@gmail.com>
2025-08-20 15:58:07 -0700

    Changed allocation of dimension

--- gsmphys/gust_parameterization.f90
+++ gsmphys/gust_parameterization.f90
@@ -3,17 +3,17 @@ subroutine compute_gust(im, u_10m, v_10m, ustar, zol, z1, gust_parameter, gust)
   implicit none
 
   integer, intent(in) :: im
-  real(kind=kind_phys), intent(in) :: u_10m(im), v_10m(im), ustar(im), &
-                                      zol(im), z1(im), gust_parameter(im)
-  real(kind=kind_phys), intent(out) :: gust(im)
+  real(kind=kind_phys), dimension(im), intent(in) :: u_10m, v_10m, ustar, &
+                                      zol, z1, gust_parameter
+  real(kind=kind_phys), dimension(im), intent(out) :: gust
 
   real(kind=kind_phys), parameter :: PBL_HEIGHT = 1000.0_kind_phys ! in meters
-  real(kind=kind_phys) :: H_o_z(im)
+  real(kind=kind_phys), dimension(im) :: H_o_z
   integer :: i
 
   ! Follows the IFS gust implementation (see Eq. 3.109 in physics documentation for cycle Cy49r1)
 
-  do i = 1,im
+  do i=1,im
     gust(i) = gust_parameter * ustar(i)
 
     H_o_z(i) = PBL_HEIGHT / z1(i)
