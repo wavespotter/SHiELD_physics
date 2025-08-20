4f176e515c2d26819ab9415557bb8d0715288e87
Wolfgang Langhans <Wolfgang.Langhans@sofarocean.com>
2025-08-20 11:29:03 -0700

    fixed something

--- gsmphys/gust_parameterization.f90
+++ gsmphys/gust_parameterization.f90
@@ -8,6 +8,7 @@ subroutine compute_gust(im, u_10m, v_10m, ustar, zol, z1, gust_parameter, gust)
   real(kind=kind_phys), intent(out) :: gust(im)
 
   real(kind=kind_phys), parameter :: PBL_HEIGHT = 1000.0_kind_phys ! in meters
+  real(kind=kind_phys) :: H_o_z
 
   ! Follows the IFS gust implementation (see Eq. 3.109 in physics documentation for cycle Cy49r1)
 
